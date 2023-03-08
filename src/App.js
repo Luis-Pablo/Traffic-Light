8a8be40206539f41949631b022dfe5946aad3b0f
Luis Pablo <pabloarias024@gmail.com>
2023-03-08 18:50:03 -0300

    semaforo terminado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import { useState } from 'react';
 
-function TrafficLight () {
+function TrafficLight() {
+  
+  
+  const color = {
+    rojo: 'bg-danger',
+    amarillo: 'bg-warning',
+    verde: 'bg-success'
+  }
+  const [ligth, setLigth] = useState(color)
+
+
   return (
     <div className='container'>
       <div className='semaforo bg-dark justify-content-center"'>
-        <div className='row  bg-danger rounded-circle'></div>
-        <div className='row bg-warning rounded-circle'></div>
-        <div className='row bg-success rounded-circle'></div>
+        <button className={`row rounded-circle ro ${ligth.rojo}`}
+          onClick={() => setLigth({rojo: 'rojo'}) }
+        >         
+        </button>
+        <button className={`row rounded-circle am ${ligth.amarillo}`}
+          onClick={() => setLigth({amarillo: 'amarillo'})}
+        >
+
+        </button>
+        <button className={`row rounded-circle ve ${ligth.verde}`}
+          onClick={() => setLigth({ verde: 'verde' })}
+        ></button>
+      </div>
+      <div>
+        
       </div>
     </div>
   )
