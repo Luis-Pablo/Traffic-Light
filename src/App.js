8c18581a7008f021903916516efc4cc56df39595
Luis Pablo <pabloarias024@gmail.com>
2023-03-07 23:49:16 -0300

    semaforo construido

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 function TrafficLight () {
   return (
     <div className='container'>
-      <div className='semaforo bg-dark p-2'>
-        <div className='row bg-danger rounded-circle my-2'></div>
+      <div className='semaforo bg-dark justify-content-center"'>
+        <div className='row  bg-danger rounded-circle'></div>
         <div className='row bg-warning rounded-circle'></div>
         <div className='row bg-success rounded-circle'></div>
       </div>
