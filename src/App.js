e4251de6798e59d1a549cb374005244dd249e0c5
Luis Pablo <pabloarias024@gmail.com>
2023-03-07 23:34:36 -0300

    semaforo creado, faltan luces y useState

--- src/App.js
+++ src/App.js
@@ -2,25 +2,20 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
-function App() {
+function TrafficLight () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='semaforo bg-dark p-2'>
+        <div className='row bg-danger rounded-circle my-2'></div>
+        <div className='row bg-warning rounded-circle'></div>
+        <div className='row bg-success rounded-circle'></div>
+      </div>
     </div>
-  );
+  )
+  
+
+
+ 
 }
 
-export default App;
+export default TrafficLight;
